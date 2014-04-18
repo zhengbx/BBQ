a676be8db13a8b0c39926fa5956cda369e312cad
bs12 <bs12@princeton.edu>
2014-04-18 16:54:55 -0400

    defined and adapted FDmetResults class

--- results.py
+++ results.py
@@ -1,15 +1,16 @@
 class FDmetResult(object):
-    def __init__(self, FragmentResults, ClusterFactor):
+    def __init__(self, FragmentResults):#, ClusterFactor):
         # sum up the total energy and total electron number        
         TotalEnergy = 0.
-        TotalElec = 0.
+        TotalnElec = 0.
         for (Fragment, r) in FragmentResults:
-            Factor = Fragment.GetTotalFactor() * ClusterFactor
-            TotalEnergy += Factor * r.EmbEnergy
-            TotalElec += Factor * r.EmbElec
+            Factor = Fragment.factor #* ClusterFactor
+            if r.EmbEnergy is not None:
+                TotalEnergy += Factor * r.EmbEnergy
+            TotalnElec += Factor * r.nEmbElec
         self.TotalElecEnergy = TotalEnergy
         self.TotalEnergy = None
-        self.TotalElec = TotalElec
+        self.TotalnElec = TotalnElec
         self.FragmentResults = FragmentResults
         self.FullSystemFock = None
         self.FullSystemVcor = None
