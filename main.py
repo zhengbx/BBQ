de23b43fa87c5b12defbfeecf4a32bbd0febdecd
bs12 <bs12@princeton.edu>
2014-04-18 16:57:15 -0400

    finish main routine to run lattice calcs (printout missing)

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import numpy as np
 import itertools as it
 
 from helpers import mdot, ExtractSpinComp, CombineSpinComps
-from vcor_fit import FitVcorComponent
 from results import FDmetResult
 from diis import FDiisContext
 from inputs import Input
@@ -19,31 +18,6 @@ from geometry import BuildLatticeFromInput, Wavefct
 from ham import Hamiltonian
 
 
-def FitCorrelationPotential(Input, GEOM, TYPE, EmbMfdHam, nEmb, RdmHl):
-    """fit a matrix nEmb x nEmb, referring to an operator connecting  
-    all embedded sites to each other, to more closely approach the 
-    correlated calculation result in with the mean-field method.
-    Returns nEmb x nEmb matrix (always), even if due to the fitting
-    criterion used the actual matrix is only a subset of this."""
-
-    nImp = GEOM.nImp
-    OrbType = GEOM.OrbType
-    ORB_OCC = GEOM.OrbOcc()
-    nElec = GEOM.nElec
-    VcorType = Input.FITTING.VcorType
-    VcorFitType = Input.FITTING.VcorFitType
-
-    if ( OrbType == "RHF" ):
-        assert(nElec%2 == 0)
-        return FitVcorComponent(EmbMfdHam, nImp, (1./ORB_OCC)*RdmHl, VcorType, VcorFitType)
-    else:
-        assert(OrbType == "UHF")
-        return CombineSpinComps(
-            FitVcorComponent(EmbMfdHam[ ::2, ::2], nImp/2, RdmHl[ ::2, ::2], 
-                            VcorType, VcorFitType),
-            FitVcorComponent(EmbMfdHam[1::2,1::2], nImp/2, RdmHl[1::2,1::2], 
-                            VcorType, VcorFitType))
-    pass
 
 
 
@@ -55,7 +29,7 @@ def main(inputdict):
     Lattice = BuildLatticeFromInput(Inp.GEOMETRY, Inp.WAVEFUNCTION.OrbType)
     HAM = Hamiltonian(Inp.HAMILTONIAN)
     WAVEFCT = Wavefct(Inp.WAVEFUNCTION, Lattice)
-    TYPE = NormalDmet(Inp.MFD, WAVEFCT.OrbType, WAVEFCT.nElec, WAVEFCT.nElecA, WAVEFCT.nElecB, WAVEFCT.Ms)
+    TYPE = NormalDmet(Inp.MFD, Inp.FITTING, WAVEFCT.OrbType, WAVEFCT.nElec, WAVEFCT.nElecA, WAVEFCT.nElecB, WAVEFCT.Ms)
   
     #print Lattice.UnitCell print function not implemented yet
     Lattice.set_Hamiltonian(HAM)
@@ -68,7 +42,7 @@ def main(inputdict):
 
 
     MfdHam = Lattice.get_h0()
-    FSCoreHam = Lattice.get_h0()
+    CoreH = Lattice.get_h0()
     Int2e = HAM.get_Int2e(Lattice, WAVEFCT.OrbType)
 
     Fragments = Lattice.supercell.fragments
@@ -77,9 +51,15 @@ def main(inputdict):
     VcorLarge = TYPE.GuessVcor(WAVEFCT.OrbType, Inp.DMET, Lattice.supercell, HAM.U)
  
     for iMacroIt in range(DmetMaxIt):
+
         MfdHam_aug = np.zeros(MfdHam.shape)
         MfdHam_aug += MfdHam
         MfdHam_aug[0] += VcorLarge
+
+        CoreH_aug = np.zeros(CoreH.shape)
+        CoreH_aug += CoreH
+        CoreH_aug[0] += VcorLarge
+               
         if HAM.transinv == True:
             # use translational symmetry
             MfdHamK_aug = Lattice.FFTtoK(MfdHam_aug) 
@@ -88,6 +68,7 @@ def main(inputdict):
         else:
             MfdResult = TYPE.RunMfd(MfdHam_aug, HamBlockDiag = False) 
             MfdRdm = MfdResult.Rdm
+        
         FragmentResults = []
         FragmentPotentials = []
         for (iFragment,Fragment) in enumerate(Fragments):
@@ -96,56 +77,43 @@ def main(inputdict):
                                              (MfdRdm.shape[0]*MfdRdm.shape[1],) 
                                              + MfdRdm.shape[2:]), 
                                              Fragment.get_sites())
-                # check EmbBasis correct
-                # trafo Int2e, n2eOrb
-                EmbHam, EmbMfdHam, Int2e, EmbRdm = TYPE.MakeEmbHam(Lattice, 
-                                                                  MfdHam_aug, MfdRdm, 
-                                                                  HAM, EmbBasis, 
-                                                                  Fragment.get_sites())
-                HlResults = TYPE.ImpSolver(EmbCoreH, EmbMfdHam, EmbRdm, 
+                EmbCoreH, EmbCoreH_aug, EmbInt2e, EmbRdm = TYPE.MakeEmbHam(Lattice, 
+                                                                          MfdHam_aug, MfdHam, 
+                                                                          MfdRdm, Int2e, EmbBasis, 
+                                                                          Fragment.get_sites())
+                HlResults = TYPE.ImpSolver(EmbCoreH, EmbRdm, 
                                           Fragment.get_emb_method(), 
-                                          Fragment.get_sites(), Int2e)
+                                          Fragment.get_sites(), EmbInt2e)
                 FragmentResults.append((Fragment, HlResults))
-                vloc = FitCorrelationPotential(Inp, GEOM, TYPE, EmbMfdHam, 
-                                              TYPE.MakeEmbBasis.nEmb, RdmHl)
+                vloc = TYPE.FitCorrelationPotential(Inp.FITTING, Fragment.get_sites(), 
+                                                   HlResults.nEmbElec, EmbCoreH_aug, 
+                                                   HlResults.Rdm)
                 FragmentPotentials.append(vloc)
-        ClusterFactor = GEOM._EnergyFactor()
-        # ^- for normalization with super-cell cluster size. Purely cosmetic.
-        FragRes = FDmetResults(FragmentResults, ClusterFactor)
-        FragRes.MeanFieldEnergy = MfdResult.MeanFieldEnergy
-        FragRes.TotalEnergy = FragRes.TotalElecEnergy + MfdResult.CoreEnergy
+        FragRes = FDmetResult(FragmentResults)#, ClusterFactor)
+        FragRes.MfdEnergy = MfdResult.Energy
+        FragRes.TotalEnergy = FragRes.TotalElecEnergy + HAM.get_core_energy(Lattice)
         FragRes.FragmentPotentials = FragmentPotentials
-        dVcorLarge = np.zeros(VcorLarge.shape)
 
-        for (Fragment, VcorFull) in zip(GEOM.Fragments, FragRes.FragmentPotentials):
-            nImp = len(Fragment.Sites)
-            Vcor = Fragment.Factor * VcorFull[:nImp,:nImp]
-            for Replica in Fragment.Replicas:
-                VcorR = mdot(Replica.Trafo, Vcor, Replica.Trafo.T)
-                E = list(enumerate(Replica.Sites))
-                for (i,iSite),(j,jSite) in it.product(E,E):
-                    dVcorLarge[iSite,jSite] += VcorR[i,j]
-
+        dVcorLarge = TYPE.VcorUpdate(FragmentPotentials, Fragments, VcorLarge.shape)
         dVsum = np.dot(dVcorLarge.flatten(),dVcorLarge.flatten())
 
         if (iMacroIt == 0 ):
-            InitialHfEnergy = FSMfdResult.MeanFieldEnergy
+            InitialHfEnergy = MfdResult.Energy
         FragRes.dVc = dVsum
         if ( dVsum < ThrdVcor ):
             print "Convergence criteria met -- done."
             break
 
         #Log("PROGESS OF CORR.POTENTIAL FIT:\n")
-
         vMfd = np.zeros((1))
-        vMfd = MfdHam[0] - FSCoreHam[0]
+        vMfd = MfdHam[0] - CoreH[0]
         SkipDiis = dVsum > DiisThr and iMacroIt < DiisStart
         VcorLarge, dVcorLarge, vMfd, c0 = dc.Apply(VcorLarge, dVcorLarge, vMfd, Skip=SkipDiis)
         if not SkipDiis:
             print "\nVcor extrapolation: DIIS{{{:2d} {:2d}  {:8.2e}}}" %(dc.nDim, dc.iNext, c0)
-        MfdHam[0] = vMfd + FSCoreHam[0]
+        MfdHam_aug[0] = vMfd + CoreH_aug[0]
         VcorLarge += dVcorLarge
-        
+       
     print "DMET SCF cycle converged after {} iterations" %(1+iMacroIt)
     print "Final residual on unit-cell vcor is {:.2e}"  %(dVsum)
 
@@ -174,12 +142,14 @@ if __name__ == '__main__':
          'ClusterSize': np.array([2, 2]),
          'LatticeSize': np.array([8, 8]),
          'Fragments': [{"Sites":range(4), "ImpSolver":'Fci',
-                        "Fitting":'FullRdm'},],
+                        "Fitting":'FullRdm', "Factor": 1},],
          'BoundaryCondition': 'pbc',},
        'DMET':
        {'init_guess_type': 'AF'},
        'MFD':
-       {'mfd_algo': 'hf'}
+       {'mfd_algo': 'diag1el'},
+       'FITTING':
+       {'v_fit_domain': 2, 'vfit_method': 'FullRdm'}
     }
     main(inpdic)
 
